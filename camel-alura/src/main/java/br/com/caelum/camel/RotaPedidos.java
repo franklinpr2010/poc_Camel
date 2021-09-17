aaddc90ba8fa2a04fa29c56fb9b1cccb62c8c538
GFT\fnpa <franklinpr2010@gmail.com>
2021-09-16 21:10:26 -0300

    fazendo subrotas no camel

---
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -0,0 +1,41 @@
+package br.com.caelum.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.mortbay.jetty.HttpSchemes;
+
+public class RotaPedidos {
+
+	public static void main(String[] args) throws Exception {
+
+		CamelContext context = new DefaultCamelContext();
+		context.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+
+				from("file:pedidos?delay=5s&noop=true").
+					setProperty("pedidoId", xpath("/pedido/id/text()")).
+					setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
+					split().
+						xpath("/pedido/itens/item").
+					filter().
+						xpath("/item/formato[text()='EBOOK']").
+					setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+					marshal().xmljson().
+					log("${id} - ${body}").
+					setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
+					setHeader(Exchange.HTTP_QUERY,simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).
+				to("http4://localhost:8080/webservices/ebook/item");
+			}
+			
+		});
+
+		context.start();
+		Thread.sleep(20000);
+		context.stop();
+	}	
+}
